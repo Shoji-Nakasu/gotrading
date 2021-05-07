f04934a2407048e7d8ee8e22504d44af24b97550
Shoji-Nakasu <santada0606@gmail.com>
2021-05-07 18:31:38 +0900

    Implement Bitflyer API Authentication and GetBalance API

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/Shoji-Nakasu/gotrading
 
 go 1.15
 
-require gopkg.in/ini.v1 v1.62.0
+require (
+	github.com/golang/protobuf v1.5.2
+	gopkg.in/ini.v1 v1.62.0
+)
--- main.go
+++ main.go
@@ -1,13 +1,16 @@
 package main
 
 import (
-	"log"
+	"fmt"
 
+	"github.com/Shoji-Nakasu/gotrading/bitflyer"
 	"github.com/Shoji-Nakasu/gotrading/config"
 	"github.com/Shoji-Nakasu/gotrading/utils"
 )
 
 func main() {
 	utils.LoggingSettings(config.Config.LogFile)
-	log.Println("test")
+	apiClient := bitflyer.New(config.Config.ApiKey, config.Config.ApiSecret)
+	fmt.Println(apiClient.GetBalance())
+	
 }
